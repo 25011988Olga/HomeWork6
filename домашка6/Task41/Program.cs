a5e4b7573699614eadcd18766666f5deb14e459d
25011988Olga <olga.moseychuk.88@mail.ru>
2022-10-21 21:19:45 +0300

    выполнила Task43

---
+++ домашка6/Task41/Program.cs
@@ -0,0 +1,18 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте,
+//сколько чисел больше 0 ввёл пользователь.
+Console.Write("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int c = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 4: ");
+int d = Convert.ToInt32(Console.ReadLine());
+int count =0;
+{
+while(a>0) count++;
+while(b>0) count++;
+while(c>0) count++;
+while(d>0) count++;
+}
+Console.WriteLine($"Кол-во элементов больше 0: {count}");
---
+++ домашка6/task43/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите числа: ");
+int n = Convert.ToInt32(Console.ReadLine());
+   
+Console.WriteLine($"Кол-во элементов больше 0: {n>0}");
